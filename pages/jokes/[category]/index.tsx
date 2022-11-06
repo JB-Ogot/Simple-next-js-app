eceda6e5214671a58f09d5debdabb27b616ae7d6
JB-Ogot <jbethwel21@gmail.com>
2022-11-06 20:58:50 +0300

    load and display random category jokes

--- pages/jokes/[category]/index.tsx
+++ pages/jokes/[category]/index.tsx
@@ -1,7 +1,43 @@
-import { useRouter } from "next/router";
+import Head from "next/head";
+import Link from "next/link";
+import Error from "next/error";
+import { Button } from "../../../components";
+import styles from "../../../styles/Home.module.css";
 
-export default function Category() {
-  const router = useRouter();
-  const { category } = router.query;
-  return <div>Category</div>;
+export default function Category({ joke, statusCode }: any) {
+  if (statusCode === 404) {
+    return <Error statusCode={statusCode} />;
+  }
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Category Joke</title>
+        <meta
+          name="description"
+          content="A next js application that generates chucknorris jokes"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>{joke.categories[0]} Joke</h1>
+      <main className={styles.main}>
+        <p className={styles.p}>{joke.value}</p>
+        <Link href="/">
+          <Button label="Back to Home Page" />
+        </Link>
+      </main>
+    </div>
+  );
 }
+
+export const getServerSideProps = async (context: any) => {
+  const response = await fetch(
+    `https://api.chucknorris.io/jokes/random?category=${context.params.category}`
+  );
+  const joke = await response.json();
+  return {
+    props: {
+      joke,
+      statusCode: response.status,
+    },
+  };
+};
